a8472b61c3d5935eae60c107e2032b173143c99a
Placide0007 <lazareplacide02@gmail.com>
2025-08-17 11:35:37 +0200

    update about page

--- resources/views/about/about.blade.php
+++ resources/views/about/about.blade.php
@@ -9,8 +9,8 @@
     {{-- section hero --}}
     
     <section id="hero">
-        <h1 class="hero-title">À propos de nous</h1>
-        <p>Passionnés de technologie, nous proposons les meilleurs produits high-tech au meilleur prix.</p>
-        <p>Qualité, innovation et service sont au cœur de notre mission.<a href="">plus d'info</a></p>
+        <h1 class="hero-title">À propos</h1>
+        <p>Passionnés de technologie, je propose les meilleurs produits high-tech au meilleur prix.</p>
+        <p>Qualité, innovation et service sont au cœur de ma mission.<a href="">plus d'info</a></p>
     </section>
 @endsection
